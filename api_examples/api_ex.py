50c0543bad78894ab3011ad8d1419819580d3ba8
yasvanth-eng <yasvanth.prabu1910@gmail.com>
2026-07-03 19:31:06 +0530

    driving class ex added

--- api_examples/api_ex.py
+++ api_examples/api_ex.py
@@ -1,17 +1,17 @@
 #need to install fastapi uvicorn
 #pip install fastapi uvicorn
+#running command uvicorn api_ex:fastapi_obj --reload
 from fastapi import FastAPI
 import uvicorn
 from yasvanth_school import yasvanth_school
+from yasvanth_school import tenth_class ,eleventh_class,twelth_class,driving
+
 
 fastapi_obj=FastAPI(title="sample api's")
 
 @fastapi_obj.get("/hello_world")
 def welcome():
     return "hello world is running"
-
-# if __name__ == "__main__":
-#     uvicorn.run()
 
 yasvanth_school_obj=yasvanth_school()
 @fastapi_obj.get("/get_all_student_record")
@@ -23,3 +23,22 @@ def get_all_student_record():
             yasvanth_school_obj.classtwelve(),
         ]
     }
+
+#tenth calss winner
+
+@fastapi_obj.post("/tenth_winner")
+def tenth_winner(std:str,sports_winner:str,sec:str):
+    tenth_class_obj=tenth_class(std,sports_winner,sec)
+    return {
+        "details":tenth_class_obj.sports_winner()
+    }
+
+@fastapi_obj.post("/eligible_or_not")
+def eligibility(person_age:int,person_name:str):
+    driving_obj=driving(age=person_age,name=person_name)
+    return {
+        "status":driving_obj.eligible_or_not(),
+        "total count":driving_obj.count_eligible()
+    }
+    
+    
--- api_examples/yasvanth_school.py
+++ api_examples/yasvanth_school.py
@@ -5,12 +5,52 @@ class yasvanth_school:
         return "class eleven 46 students"
     def classtwelve(self):
         return "class twelve 42 students"
-    
 
-yasvanth_school_obj=yasvanth_school()
+class management:
+    def __init__(self,std:str,sports_winner:str,sec:str):
+        self.name="yes public international school"
+        self.std=std
+        self.sports=sports_winner
+        self.sec=sec
+    def sports_winner(self):
+        return f"{self.name} participated in {self.std} section {self.sec} sports {self.sports}"
 
-print(yasvanth_school_obj.classten())
 
-print(yasvanth_school_obj.classeleven())
+class tenth_class(management):
+    def __init__(self, std, sports_winner, sec):
+        super().__init__(std, sports_winner, sec)
 
-print(yasvanth_school_obj.classtwelve())
+
+class eleventh_class(management):
+    def __init__(self, std, sports_winner, sec):
+        super().__init__(std, sports_winner, sec)
+
+
+class twelth_class(management):
+    def __init__(self, std, sports_winner, sec):
+        super().__init__(std, sports_winner, sec)
+
+
+#driving eligible or not
+
+class driving:
+    def __init__(self,age:int,name:str):
+        self.age=age
+        self.name=name
+        self.age_list=[13,14,23,7,35]
+        
+    def eligible_or_not(self):
+        if self.age>=18:
+            return f"{self.name} eligible for licence" 
+        else:
+            return f"{self.name} not eligible for licence"
+
+    def count_eligible(self):
+        count=0
+        for i in self.age_list:
+            if i >18:
+                count+=1
+
+        return count
+
+        
